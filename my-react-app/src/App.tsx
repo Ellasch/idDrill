a55689ab8e05bbd633862e3e7d5b39df45f5a249
= <97621005+shlomi991@users.noreply.github.com>
2024-11-21 11:13:00 +0200

    feat(App.tsx): create base for client

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,34 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import PinInput from 'react-pin-input'
+import { Button } from 'react-bootstrap'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [digits, setDigits] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div style={{display: 'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+        <p style={{fontSize:40, fontWeight:'bold'}}>Enter the first 8 digits of your id</p>
+
+        <PinInput 
+          length={8}
+          initialValue=""
+          secret
+          secretDelay={100} 
+          onChange={(value, index) => {}} 
+          type="numeric" 
+          inputMode="number"
+          style={{padding: '20px'}}  
+          inputStyle={{borderColor: 'black'}}
+          inputFocusStyle={{borderColor: 'purple'}}
+          onComplete={(value, index) => {}}
+          autoSelect={true}
+          regexCriteria={/^[ A-Za-z0-9_@./#&+-]*$/}
+        />
+
+        <Button variant='danger' 
+          style={{marginTop:40, fontSize:30, color:'white', backgroundColor:'purple'}}
+          >Send</Button>
+        </div>
     </>
+
+
   )
 }
 
