17dc55803af624fc84f88a77e2fa41c4f7ea0fb8
= <97621005+shlomi991@users.noreply.github.com>
2024-11-24 14:03:18 +0200

    feat(client/App.tsx, server/routes/index.ts): change pinInput so it will allow ful paste. Also change input type to array

--- my-react-app/client/src/App.tsx
+++ my-react-app/client/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { useState } from 'react'
 import './App.css'
-import PinInput from 'react-pin-input'
+import { PinInput } from 'react-input-pin-code';
 import { Button } from 'react-bootstrap'
 import axios from 'axios'
 
 const baseUrl = 'http://localhost:3000'
 
 function App() {
-  const [digits, setDigits] = useState("");
+  const [digits, setDigits] = useState(new Array<string>(8).fill(""));
   // const [clicked, setClicked] = useState(false);
   const [lastDigit, setLastDigit] = useState(0);
 
@@ -16,29 +16,26 @@ function App() {
       <div style={{display: 'flex', flexDirection:'column', alignItems:'center', marginTop:'20vh'}}>
         <p style={{fontSize:40, fontWeight:'bold'}}>Enter the first 8 digits of your id</p>
 
-        <PinInput 
-          length={8}
-          initialValue=""
-          onChange={(value, index) => {setDigits(value)}} 
-          type="numeric" 
-          inputMode="number"
-          style={{padding: '20px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}  
-          inputStyle={{borderColor: 'black'}}
-          inputFocusStyle={{borderColor: 'purple'}}
-          onComplete={(value, index) => {}}
-          autoSelect={true}
-          regexCriteria={/^[ A-Za-z0-9_@./#&+-]*$/}
-        />
+         <PinInput 
+         values={digits}
+         onChange={(value, index, values) => setDigits(values)}
+         type='number'
+         borderColor='pink'
+         placeholder=''
+         size='lg'
+         focusBorderColor='pink'
+         >
+         </PinInput>
 
         <Button 
         style={{fontSize: 30, marginTop:20
           ,borderRadius:10, padding: '10px 20px', color: 'pink', borderColor: 'pink'}}
            id='send_btn'
            onClick={async() => {
-            console.log(digits);
-           setLastDigit((await axios.get(`${baseUrl}/${digits}`)).data);
+            setLastDigit((
+            await axios.post(`${baseUrl}`,{digits})).data);
           }}
-          disabled={digits.length !== 8}
+          disabled={isArrayFull(digits) ? false : true}
            >Send</Button>
 
          {
@@ -51,6 +48,10 @@ function App() {
          }
         </div>
   )
+}
+
+const isArrayFull = (arr : Array<string>) => {
+  return arr.every((digit) => digit !== "");
 }
 
 export default App
