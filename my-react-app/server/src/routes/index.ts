94ab26ff9d78f74d81df0096b0ccfb27fea14994
= <97621005+shlomi991@users.noreply.github.com>
2024-11-24 10:54:06 +0200

    feat(index.ts): finish calculation function

--- my-react-app/server/src/routes/index.ts
+++ my-react-app/server/src/routes/index.ts
@@ -5,25 +5,27 @@ const router = Router();
 router.get('/:8Digits', (req, res) => {
   const first8 = req.params['8Digits'];
   
-  const sum = calcLast(first8);
-  res.send(`sum: ${sum}`);
+  const last = calcLast(first8);
+  res.send(`last: ${last}`);
 });
 
 const calcLast = (first8: string) => {
   const first8Arr: Array<number> = first8.split('').map(digit => parseInt(digit));
   
- first8Arr.forEach((digit, index) => {
-    const multiplier = index % 2 === 0 ? 1 : 2;
+ const newArr = first8Arr.map((digit, index) => {
+    const multiplier = index % 2 == 0 ? 1 : 2;
     digit =  digit * multiplier;
 
     if (digit > 9) {
-      digit = digit / 10 + digit % 10;
+      digit = Math.trunc(digit / 10) + digit % 10;
     }
+
+    return digit;
  })
 
- const sum = first8Arr.reduce((last, curr) => last + curr, 0);
+ const sum = newArr.reduce((last, curr) => last + curr, 0);
 
- return sum;
+ return Math.trunc(sum/10) * 10 + 10 - sum;
 }
 
 export default router;
